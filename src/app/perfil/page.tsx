11ee36881ba4bbc7021daea1ea318ea9f5093bac
Liliana Quillca Medina <liliquillcamedina@gmail.com>
2026-07-13 12:56:42 -0500

    perfil creado rapido

---
+++ src/app/perfil/page.tsx
@@ -0,0 +1,10 @@
+export default function Perfil() {
+  return (
+    <div>
+      <h1>Perfil Transportista</h1>
+      <p>Nombre: Napoleon Perez</p>
+      <p>Vehiculo: Camion</p>
+      <p>Estado: Activo</p>
+    </div>
+  );
+}
