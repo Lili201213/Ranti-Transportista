c47789643e0f459943cb1f76b68ec0de42529c50
Brenda457674 <merinobrenda546@gmail.com>
2026-07-20 10:46:20 -0500

    Eliminar Navbar duplicado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar, Sidebar } from "@/src/components/layout";
+import { Sidebar } from "@/src/components/layout";
 import { GananciasProvider } from "@/src/context/GananciasContext";
 
 const geistSans = Geist({
@@ -31,13 +31,17 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
       >
         <GananciasProvider>
-          <Navbar />
+
           <div className="mx-auto flex max-w-6xl">
+
             <Sidebar />
+
             <main className="min-w-0 flex-1 px-4 py-8 md:px-8">
               {children}
             </main>
+
           </div>
+
         </GananciasProvider>
       </body>
     </html>
