5ec335263a85b10e640ecd1af7eb5699705fe6fd
Brenda457674 <merinobrenda546@gmail.com>
2026-07-20 17:47:22 -0500

    Corrige carga del mapa de entregas

--- src/components/entregas/mapa/MapaEntrega.tsx
+++ src/components/entregas/mapa/MapaEntrega.tsx
@@ -1,48 +1,14 @@
 "use client";
 
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-} from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+import dynamic from "next/dynamic";
+
+const Mapa = dynamic(
+  () => import("./Mapa"),
+  {
+    ssr: false,
+  }
+);
 
 export default function MapaEntrega() {
-
-  const posicion: [number, number] = [
-    -15.327,
-    -74.575
-  ];
-
-  return (
-    <div className="mt-6 rounded-xl overflow-hidden border">
-
-      <h3 className="font-bold p-4">
-        🗺️ Ubicación de entrega
-      </h3>
-
-      <MapContainer
-        center={posicion}
-        zoom={13}
-        style={{
-          height: "350px",
-          width: "100%",
-        }}
-      >
-
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-
-        <Marker position={posicion}>
-          <Popup>
-            Punto de entrega
-          </Popup>
-        </Marker>
-
-      </MapContainer>
-
-    </div>
-  );
+  return <Mapa />;
 }
